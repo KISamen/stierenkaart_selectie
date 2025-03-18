6266dd97b1cbb73691eafbf7affb1fd55599fb61
Veerle van Rooijen <v.vanrooijen@kisamen.com>
2025-03-18 11:48:03 +0100

    fix titel stiercode v3

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import io
 st.title("Stierenkaart Generator")
 st.write("Upload de volgende bestanden om de stierenkaart te genereren:")
 
+# Bestandsuploaders voor de verschillende bronbestanden
 uploaded_crv = st.file_uploader("Upload Bronbestand CRV (bijv. 'Bronbestand CRV DEC2024.xlsx')", type=["xlsx"])
 uploaded_joop = st.file_uploader("Upload Bronbestand Joop Olieman (bijv. 'Bronbestand Joop Olieman.xlsx')", type=["xlsx"])
 uploaded_pim = st.file_uploader("Upload Pim K.I. Samen (bijv. 'Pim K.I. Samen.xlsx')", type=["xlsx"])
@@ -21,25 +22,25 @@ if st.button("Genereer Stierenkaart"):
             df_pim = pd.read_excel(uploaded_pim)
             df_prijs = pd.read_excel(uploaded_prijs)
             
-            # Stel een standaard merge key in
+            # Definieer de standaard merge key
             standard_key = "KI-code"
-            key_variants = ["KI-code", "KI code", "KI-Code", "ki code"]
             
-            # Zoek naar de merge key in het CRV-bestand en hernoem naar de standaard key
+            # Zoek naar varianten in het CRV-bestand
+            key_variants = ["KI-code", "KI code", "KI-Code", "ki code"]
             found_key = None
             for variant in key_variants:
                 if variant in df_crv.columns:
                     found_key = variant
                     break
             if not found_key:
-                st.error("De kolom 'KI-code' (of een variant) is niet aanwezig in het CRV-bestand. Zorg voor consistente data.")
+                st.error("Geen merge key gevonden in het CRV-bestand. Zorg dat een variant van 'KI-code' aanwezig is.")
                 st.stop()
             if found_key != standard_key:
                 df_crv.rename(columns={found_key: standard_key}, inplace=True)
             
             st.write(f"Gebruik merge key: **{standard_key}**")
             
-            # Voor het Joop Olieman-bestand: zoek en hernoem de merge key indien aanwezig
+            # In het Joop Olieman-bestand: zoek naar dezelfde merge key-varianten
             found_key_joop = None
             for variant in key_variants:
                 if variant in df_joop.columns:
@@ -49,17 +50,31 @@ if st.button("Genereer Stierenkaart"):
                 if found_key_joop != standard_key:
                     df_joop.rename(columns={found_key_joop: standard_key}, inplace=True)
             else:
-                st.warning(f"In het Joop Olieman-bestand is geen kolom gevonden die overeenkomt met '{standard_key}'.")
+                st.warning("In het Joop Olieman-bestand is geen merge key gevonden.")
             
-            # Voor het Pim K.I. Samen-bestand: hernoem 'stiecode' naar de standaard merge key
-            if "stiecode" in df_pim.columns:
-                df_pim.rename(columns={"stiecode": standard_key}, inplace=True)
+            # In het Pim K.I. Samen-bestand: zoek naar varianten voor de sleutel
+            pim_key_variants = ["stiecode", "stiercode", "Stiecode", "Stiercode"]
+            found_key_pim = None
+            for variant in pim_key_variants:
+                if variant in df_pim.columns:
+                    found_key_pim = variant
+                    break
+            if found_key_pim:
+                if found_key_pim != standard_key:
+                    df_pim.rename(columns={found_key_pim: standard_key}, inplace=True)
             else:
-                st.warning("In het Pim K.I. Samen-bestand ontbreekt de kolom 'stiecode'.")
+                st.warning("In het Pim K.I. Samen-bestand ontbreekt de kolom 'stiecode' of 'stiercode'.")
             
-            # Voor het Prijslijst-bestand: hernoem 'artikelnummer' naar de standaard merge key
-            if "artikelnummer" in df_prijs.columns:
-                df_prijs.rename(columns={"artikelnummer": standard_key}, inplace=True)
+            # In het Prijslijst-bestand: zoek naar varianten voor 'artikelnummer'
+            prijslijst_key_variants = ["artikelnummer", "Artikelnummer", "artikel nummer"]
+            found_key_prijs = None
+            for variant in prijslijst_key_variants:
+                if variant in df_prijs.columns:
+                    found_key_prijs = variant
+                    break
+            if found_key_prijs:
+                if found_key_prijs != standard_key:
+                    df_prijs.rename(columns={found_key_prijs: standard_key}, inplace=True)
             else:
                 st.warning("In het Prijslijst-bestand ontbreekt de kolom 'artikelnummer'.")
             
@@ -69,21 +84,21 @@ if st.button("Genereer Stierenkaart"):
             if standard_key in df_joop.columns:
                 df_merged = pd.merge(df_merged, df_joop, on=standard_key, how='left')
             else:
-                st.warning(f"Merge key '{standard_key}' niet gevonden in het Joop Olieman-bestand.")
+                st.warning("Merge key niet gevonden in het Joop Olieman-bestand.")
             
             if standard_key in df_pim.columns:
                 df_merged = pd.merge(df_merged, df_pim, on=standard_key, how='left')
             else:
-                st.warning(f"Merge key '{standard_key}' niet gevonden in het Pim K.I. Samen-bestand.")
+                st.warning("Merge key niet gevonden in het Pim K.I. Samen-bestand.")
             
             if standard_key in df_prijs.columns:
                 df_merged = pd.merge(df_merged, df_prijs, on=standard_key, how='left')
             else:
-                st.warning(f"Merge key '{standard_key}' niet gevonden in het Prijslijst-bestand.")
+                st.warning("Merge key niet gevonden in het Prijslijst-bestand.")
             
             # Genereer een Excelbestand met de sheetnaam 'fokstieren'
             output = io.BytesIO()
-            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            with pd.ExcelWriter(output, engine='openpyxl') as writer:
                 df_merged.to_excel(writer, sheet_name='fokstieren', index=False)
             output.seek(0)
             
